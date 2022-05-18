0a6cc91c45e915b2f1b31023de5e29e6f9d3c221
Branislav Trstenský <bt7s7k7@hotmail.sk>
2022-05-18 19:01:02 +0200

    Function to check if object is disposed

--- src/eventLib/Disposable.ts
+++ src/eventLib/Disposable.ts
@@ -2,11 +2,13 @@
 export const DISPOSE = Symbol("dispose")
 export const AUTO_DISPOSE = Symbol("autoDispose")
 
+function doubleDisposeGuard() {
+    throw new Error("Object was disposed already")
+}
+
 export function disposeObject<T extends object>(object: T) {
     if (DISPOSE in object) {
-        (object as { [DISPOSE]: any })[DISPOSE] = () => {
-            throw new Error("Object was disposed already")
-        }
+        (object as { [DISPOSE]: any })[DISPOSE] = doubleDisposeGuard
     }
 
     for (const [key, property] of Object.entries(object)) {
@@ -14,6 +16,10 @@ export function disposeObject<T extends object>(object: T) {
             property[DISPOSE]()
         }
     }
+}
+
+export function isObjectDisposed(object: IDisposable) {
+    return object[DISPOSE] == doubleDisposeGuard
 }
 
 export interface IDisposable {
